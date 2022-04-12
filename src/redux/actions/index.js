4e79c50fe876c756b2ad145aefda923e136177a0
unknown <juanignaciodelossantos01@gmail.com>
2022-04-12 09:42:42 -0300

    feat(client/action creator getEvents): add action creator getEvents and reducer

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,8 +1,14 @@
 //Here we import all de actions creator:
 import ejemplo1 from './actionEj1';
+import getEvents, { GET_EVENTS } from './getEvents';
+
+export const actions = {
+    GET_EVENTS,
+}
 
 const actionsCreator = {
     ejemplo1,
+    getEvents
 }
 
 export default actionsCreator;
