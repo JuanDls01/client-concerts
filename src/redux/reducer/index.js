a4302e76ff542e9c6468ed58e3044684777b2f9f
nicolas palomares <nicolaspalomares360@gmail.com>
2022-04-12 19:30:15 -0300

    creacion de filtro para genero feat(client/src):se agrega filtrado por genero)

--- src/redux/reducer/index.js
+++ src/redux/reducer/index.js
@@ -1,28 +1,42 @@
 import { GET_EVENTS } from "../actions/getEvents";
+import { FILT_BY_GENRE } from "../actions/filtByGenre";
+import { GET_GENRES_TYPES } from "../actions/getGenresTypes";
 
 const initialState = {
-    events: [],
-    details: []
-}
+  events: [],
+  details: [],
+  allEvents: [],
+  genres: [],
+};
 
 const rootReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case GET_EVENTS: {
-            console.log(action.payload);
-            return {
-                ...state,
-                events: action.payload,
-            }
-        }
-        case "FILT_BY_DATEs": {
-            return {
-                ...state,
-                events: action.payload,
-            };
-        }
-        default:
-          return state;
-      }
-}
+  switch (action.type) {
+    case GET_EVENTS: {
+      console.log(action.payload);
+      return {
+        ...state,
+        events: action.payload,
+        allEvents: action.paylaod,
+      };
+    }
+    case GET_GENRES_TYPES: {
+      return {
+        ...state,
+        genres: action.payload,
+      };
+    }
+    case FILT_BY_GENRE: {
+      const allEvents = state.allEvents;
+      const filterEvents = allEvents.filter((event) => event.artist.genre.name === action.payload);
+      console.log(filterEvents)
+      return {
+        ...state,
+        events: filterEvents,
+      };
+    }
+    default:
+      return state;
+  }
+};
 
 export default rootReducer;
