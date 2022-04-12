ad758d2e320630aad34b270e14f2ecc7faaf423a
JuanDls01 <juanignaciodelossantos01@gmail.com>
2022-04-12 17:05:55 -0300

    feat(client/NavBar): inicializando navBar con sus elementos

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import style from './NavBar.module.css';
+
+const NavBar = () => {
+    return (
+        <div className={style.navBarContainer}>
+            <div className={style.link}>Eventos</div>
+            <div className={style.link}>Contact</div>
+            <div className={style.link}>Registrar</div>
+            <button className={style.link}>Login</button>
+        </div>
+    )
+}
+
+export default NavBar;
