890ecda466500b04e56ee7afbf3f0d316cb9d5d5
JuanDls01 <juanignaciodelossantos01@gmail.com>
2022-04-12 18:11:59 -0300

    feat(client/NavBar): añadiendo posicionamiento

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,10 +4,12 @@ import style from './NavBar.module.css';
 const NavBar = () => {
     return (
         <div className={style.navBarContainer}>
-            <div className={style.link}>Eventos</div>
-            <div className={style.link}>Contact</div>
-            <div className={style.link}>Registrar</div>
-            <button className={style.link}>Login</button>
+            <div className={style.items}>
+                <div className={style.link}>Eventos</div>
+                <div className={style.link}>Contact</div>
+                <div className={style.link}>Registrar</div>
+                <button className={style.link}>Login</button>
+            </div>
         </div>
     )
 }
