420da3d4aedbf03dc567d8469a04b024de622185
nicolas palomares <nicolaspalomares360@gmail.com>
2022-04-14 14:45:19 -0300

    feat(src/components/Filters/Filter.jsx):se agrega filtro por genero y fecha

--- src/components/Filters/Filter.jsx
+++ src/components/Filters/Filter.jsx
@@ -2,74 +2,52 @@ import React, { useState } from "react";
 import DatePicker from "react-datepicker";
 import { useDispatch, useSelector } from "react-redux";
 import { filtEvents } from "../../redux/actions/actionFiltEvents";
-import moment from "moment"
-
-
 
 export default function Filter() {
 
-  // let startDate =new Date("")
-
-
   const dispatch = useDispatch();
 
-  const genres= useSelector(state => state.genres)
+  const genres = useSelector((state) => state.genres);
 
-  // const [genre,setGenre]=useState("")
+  const [genre, setGenre] = useState("");
 
-  // const [startDate, setStartDate] = useState(new Date());
+  const [startDate, setStartDate] = useState(new Date());
 
-  // const [endDate, setEndDate] = useState(null);
-
-  const [datos,setDatos] = useState({
-    genre:"",
-    startDate:new Date(),
-    endDate:null
-  })
-
-  const onChange = (dates) => {
+  const [endDate, setEndDate] = useState(null);
 
 
-    setDatos({
-      ...datos,
-      startDate: dates[0],
-      endDate:dates[1] 
-    })
+  const onChange = async (dates) => {
+    const [start, end] = dates;
+  
+    setStartDate(start)
+    setEndDate(end)
 
-
-    dispatch(filtEvents(datos))
+    dispatch(filtEvents({start,end,genre}));
   };
 
-  function handleOnChange(e){
-    setDatos({
-      ...datos,
-      genre:e.target.value
-    })
-    dispatch(filtEvents(datos))
+  function handleOnChange(e) {
+    setGenre(e.target.value)
+    dispatch(filtEvents({startDate,endDate,genre}));
   }
 
   return (
     <div>
       {/* {COMPONENTE DE CALENDARIO} */}
-    <DatePicker
-      selected={datos.startDate}
-      onChange={(e)=>onChange(e)}
-      startDate={datos.startDate}
-      endDate={datos.endDate}
-      selectsRange
-      dateFormat="yyyy-MM-dd"
-      inline
-    />
-    {/* {COMPONENTE DE SELECTOR DE GENERO} */}
+      <DatePicker
+        selected={startDate}
+        onChange={onChange}
+        startDate={startDate}
+        endDate={endDate}
+        selectsRange
+        inline
+      />
+      {/* {COMPONENTE DE SELECTOR DE GENERO} */}
 
-    <select onChange={(e)=>handleOnChange(e)}>
-      {genres.map((el)=>(
-        <option value={datos.genre}>{el}</option>
-      ))}
-    </select>
-
+      <select onChange={(e) => handleOnChange(e)}>
+        {genres && genres.map((el) => (
+          <option value={el}>{el}</option>
+        ))}
+      </select>
     </div>
-
-    
   );
 }
