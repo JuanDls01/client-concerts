9b6fd6a2352487301e61092def31c8d0f4b98aaa
davidvazgon26 <davidvazgon@hotmail.com>
2022-04-14 16:10:59 -0500

    mostrar menu principal y avance del footer

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,7 @@
 
 //Componentes
 import Hero from '../Hero/Hero';
-import EventDetail from '../EventDetail/EventDetail';
+// import EventDetail from '../EventDetail/EventDetail';
 
 import EventsCards from '../EventsCards/EventsCards';
 import NavBar from '../NavBar/NavBar';
@@ -13,9 +13,8 @@ import s from "./Home.module.css"
 export default function Home(){
     return (
         <div>
-            <NavBar/>
             <Hero/>
-            <EventDetail/>
+            <NavBar/>
             <EventsCards/>
             <Footer/>
         </div>
