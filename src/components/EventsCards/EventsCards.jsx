418225bddbfc254007200d1ddcd2a36afd81dcb2
JuanDls01 <juanignaciodelossantos01@gmail.com>
2022-04-14 13:07:29 -0300

    renderiza y hace bien el request de los eventos

--- src/components/EventsCards/EventsCards.jsx
+++ src/components/EventsCards/EventsCards.jsx
@@ -5,6 +5,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import actionsCreator from '../../redux/actions/index';
 import EventCard from '../EventCard/EventCard';
 import Paginated from '../Paginated/Paginated';
+import style from './EventsCards.module.css'
 
 const INITIALPAGE = 0;
 const EVENTSPERPAGE = 6;
@@ -15,11 +16,13 @@ const EventsCards = () => {
 
     const {getEvents} = actionsCreator;
     useEffect(()=>{
+        console.log('hola')
         dispatch(getEvents());
     },[])
 
     // Eventos traídos del estado global: 
     const events = useSelector((state) => state.events);
+    console.log('events',events)
 
     // Estado que indica la página actual:
     const [currentPage, setCurrentPage] = useState(INITIALPAGE);
@@ -27,10 +30,11 @@ const EventsCards = () => {
     const pagesVisited = currentPage*EVENTSPERPAGE;
     // Array de las cartas que renderizamos en la página actual:
     const currentEvents = events.slice(pagesVisited, pagesVisited + EVENTSPERPAGE);
-    
+    if(currentEvents.length>0){console.log('currentEvents.artist.name', currentEvents[0].Artist.name)}
+
     return (
-        <div>
-            <div>
+        <div className={style.EventsPaginatedContainner}>
+            <div className={style.EventsCardsContainner}>
                 {currentEvents && currentEvents.map(event => {
                     return <EventCard 
                         key={event.id}
@@ -38,9 +42,9 @@ const EventsCards = () => {
                         name={event.name}
                         imgEvent={event.imgEvent}
                         date={event.date}
-                        artistName={event.artist.name}
-                        stageName={event.stage.name}
-                        location={event.stage.location}
+                        artistName={event.Artist.name}
+                        stageName={event.Stage.name}
+                        // location={event.stage.location}
                     />
                 })}
             </div>
--- src/redux/actions/getEvents.js
+++ src/redux/actions/getEvents.js
@@ -4,7 +4,8 @@ export const GET_EVENTS = 'GET_EVENTS';
 
 const getEvents = () => async (dispatch) => {
     try {
-        const json = await axios.get('http://localhost:3001/events');
+        console.log('entre almenos')
+        const json = await axios.get('http://localhost:3001/event');
         console.log(json);
         const events = json.data;
         return dispatch({
