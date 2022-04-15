51bdffc8ead9be2158728c408bad2987fd2e63e0
Ernest2104 <villagra9@gmail.com>
2022-04-15 00:34:40 -0300

    actualizar feat/detail

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import './App.css';
-// import EventDetail from './components/EventDetail';
-import SearchBars from './components/SearchBars';
 
 function App() {
   return (
     <div className="App">
       <h1>PF - GRUPO 2</h1>
-      {/* <EventDetail /> */}
-      {/* <SearchBars /> */}
     </div>
   );
 }
