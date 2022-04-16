e08c67ba6edf543b831d5b3d4f010b9d1ced70b7
Ernest2104 <villagra9@gmail.com>
2022-04-16 13:58:18 -0300

    searchbar actualizado

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 //import { Route, Routes } from 'react-router-dom';
-import SearchBars from './components/SearchBars/SearchBars.jsx'
+//import SearchBars from './components/SearchBars/SearchBars.jsx'
 
 function App() {
   return (
     <div>
-      <SearchBars />
+      
     </div>
   );
 }
