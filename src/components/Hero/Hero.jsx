f4f36191dee99e2628fbaa372932227e236ddfe2
JuanDls01 <juanignaciodelossantos01@gmail.com>
2022-04-18 09:20:53 -0300

    Posicionamiento de los componentes de la LandingPage

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,14 +7,21 @@ export default function Hero(){
 
     return (
     <div className={s.externo}>
-        
-        <img src={logo} className={s.imagen}/>
-        <h4 className={s.titulo}>My ticket is the site with the best fashion events and your favorite artists</h4>
-        <p className={s.parrafo}>Find the best events, artists, concerts, plays and more. Plus everything from the comfort of your home. Search them by place, date, artist or genre </p>
-        <ul>
-            <li className={s.btn1}>Get Ticket</li>
-            <li className={s.btn2}>Learn More</li>
-        </ul>
+        <div className={s.imgContainner}>
+            <img src={logo} className={s.imagen}/>
+        </div>
+        <div className={s.infoContainner}>
+            <h4 className={s.titulo}>My ticket is the site with the best fashion events and your favorite artists</h4>
+            <p className={s.parrafo}>Find the best events, artists, concerts, plays and more. Plus everything from the comfort of your home. Search them by place, date, artist or genre </p>
+            <div className={s.bttns}>
+                <button className={s.btn1}>Get Ticket</button>
+                <button className={s.btn2}>Learn More</button>
+            </div>
+            {/* <ul>
+                <li className={s.btn1}>Get Ticket</li>
+                <li className={s.btn2}>Learn More</li>
+            </ul> */}
+        </div>
     </div>
     )
 }
