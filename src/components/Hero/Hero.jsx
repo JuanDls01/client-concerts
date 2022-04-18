a0d06ce05341dcd95437723684460873944645b6
JuanDls01 <juanignaciodelossantos01@gmail.com>
2022-04-18 08:31:00 -0300

    Pude hacer que se vea el fondo

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,9 +5,9 @@ import logo from '../../assets/images/logotipo.png'
 export default function Hero(){
 
 
-    return (<div className={s.externo}>
-        <div className={s.body}>
-        </div>
+    return (
+    <div className={s.externo}>
+        
         <img src={logo} className={s.imagen}/>
         <h4 className={s.titulo}>My ticket is the site with the best fashion events and your favorite artists</h4>
         <p className={s.parrafo}>Find the best events, artists, concerts, plays and more. Plus everything from the comfort of your home. Search them by place, date, artist or genre </p>
