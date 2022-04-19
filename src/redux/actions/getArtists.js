8ce47347abad1cf17fcb8fec4002413aa7a6c835
Jorge Vega <vegajor1112@gmail.com>
2022-04-19 01:43:36 -0300

    Creación del formulario de eventos

---
+++ src/redux/actions/getArtists.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+export const GET_ARTISTS = "GET_ARTISTS";
+
+const getArtists = () => {
+  return async (dispatch) => {
+    try {
+      const artists = await axios.get("http://localhost:3001/artist");
+      return dispatch({
+        type: GET_ARTISTS,
+        payload: artists.data,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+};
+
+export default getArtists;
