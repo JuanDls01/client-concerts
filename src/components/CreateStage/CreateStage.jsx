c5b4a401e9234e304dcb0746331a91d925062dd5
Ernest2104 <villagra9@gmail.com>
2022-04-19 21:22:27 -0300

    Finalizado componente CreateEvent

--- src/components/CreateStage/CreateStage.jsx
+++ src/components/CreateStage/CreateStage.jsx
@@ -2,10 +2,10 @@
 import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import { useDispatch } from 'react-redux';
-import { Link } from 'react-router-dom';
 import actionsCreator from '../../redux/actions';
-import style from './CreateStage.module.css'
+import style from './CreateStage.module.css';
 import Swal from 'sweetalert2';
+import FormBttn from '../Common/FormBttn/FormBttn';
 
 const { postStage } = actionsCreator;
 
@@ -48,7 +48,7 @@ const validateInput = (input) => {
   return errors;
 }
 
-export const CreateStage = ({onClose}) => {
+export const CreateStage = ({ onClose }) => {
   const dispatch = useDispatch();
   const [errors, setErrors] = useState({});
   const [input, setInput] = useState({
@@ -78,7 +78,7 @@ export const CreateStage = ({onClose}) => {
   const handleSubmit = (e) => {
     e.preventDefault();
     dispatch(postStage(input))
-    Swal.fire({ title: 'Succes!', 
+    Swal.fire({ title: 'Success!', 
                 text: 'Stage create succesful!', 
                 icon: 'success', 
                 confirmButtonText: 'Ok'
@@ -95,8 +95,10 @@ export const CreateStage = ({onClose}) => {
 
   const nextFocus = (inputF, inputS) => {
     document.getElementById(inputF).addEventListener('keydown', (event) => {
-      if (event.key === "Enter") {
+      if (event.key == "Enter") {
+        event.preventDefault();
         document.getElementById(inputS).focus();
+        event.preventDefault();
       }
     })
   }
@@ -105,31 +107,30 @@ export const CreateStage = ({onClose}) => {
     <div className={style.container}>
     <p className={style.title}>Add a new Stage</p>
     <form onSubmit={(e) => handleSubmit(e)}>
-      <p><label className={style.label}>Name: </label>
-          <input className={style.input} type="text" value={input.name} name="name" onChange={handleChange} id="inputName" onKeyDown={() => nextFocus('inputName', 'inputCapacity')}/></p>
+      <p><input placeholder='Name...' className={style.input} type="text" value={input.name} name="name" onChange={handleChange} id="inputName" onKeyDown={() => nextFocus('inputName', 'inputCapacity')}/></p>
           {errors.name && <p className={style.error}>{errors.name}</p>}
-      <p><label className={style.label}>Capacity: </label>
-          <input className={style.input} type="number" value={input.capacity} name="capacity" onChange={handleChange} id="inputCapacity" onKeyDown={() => nextFocus('inputCapacity', 'inputAddress')}/></p>
+      <p><input placeholder='Capacity...' className={style.input} type="number" value={input.capacity} name="capacity" onChange={handleChange} id="inputCapacity" onKeyDown={() => nextFocus('inputCapacity', 'inputAddress')}/></p>
           {errors.capacity && <p className={style.error}>{errors.capacity}</p>}
-      <p><label className={style.label}>address: </label>
-          <input className={style.input} type="text" value={input.address} name="address" onChange={handleChange} id="inputAddress" onKeyDown={() => nextFocus('inputAddress', 'inputLat')}/></p>
+      <p><input placeholder='Address...' className={style.input} type="text" value={input.address} name="address" onChange={handleChange} id="inputAddress" onKeyDown={() => nextFocus('inputAddress', 'inputLat')}/></p>
           {errors.address && <p className={style.error}>{errors.address}</p>}
-      <p><label className={style.label}>Coordinates(Lat): </label>
-          <input className={style.input}type="text" value={input.lat} name="lat" onChange={handleChange} id="inputLat" onKeyDown={() => nextFocus('inputLat', 'inputLon')}/></p>
+      <p><input placeholder='Coordinates(Lat)...' className={style.input}type="text" value={input.lat} name="lat" onChange={handleChange} id="inputLat" onKeyDown={() => nextFocus('inputLat', 'inputLon')}/></p>
           {errors.lat && <p className={style.error}>{errors.lat}</p>}
-      <p><label className={style.label}>Coordinates(Lon): </label>
-          <input className={style.input} type="text" value={input.lon} name="lon" onChange={handleChange} id="inputLon" onKeyDown={() => nextFocus('inputLon', 'inputDescription')}/></p>
+      <p><input placeholder='Coordinates(Lon)...' className={style.input} type="text" value={input.lon} name="lon" onChange={handleChange} id="inputLon" onKeyDown={() => nextFocus('inputLon', 'inputDescription')}/></p>
           {errors.lon && <p className={style.error}>{errors.lon}</p>}
-      <p><label className={style.label}>Description: </label>
-          <textarea className={style.input} value={input.description} name="description" onChange={handleChange} id="inputDescription"/></p>
-      <button
+      <p><textarea placeholder='Description...' rows="5" className={style.textarea} value={input.description} name="description" onChange={handleChange} id="inputDescription"/></p>
+      {/* <button
         className={style.button}
         type="submit"
         disabled={
             !input.name || !input.capacity || !input.address || errors.name || errors.capacity || errors.address || errors.lat || errors.lon ? true : false
         }>Create!
-      </button>
-        <button type="button" className={style.button} onClick={onClose}>Close</button>
+      </button> */}
+      <FormBttn
+        firstValue={input.name}
+        inputErros={errors}
+        text={'Create!'}
+      />
+      <button type="button" className={style.button} onClick={onClose}>Close</button>
     </form>
     </div>, 
     document.getElementById('portal'))
