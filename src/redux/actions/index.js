706b45465d749fc410bfe791a055bf1f5b7dd01f
Juan De los Santos <juanignaciodelossantos01@gmail.com>
2022-04-19 22:43:22 -0300

    Agrego cosas que el pull estaba eliminando

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -7,6 +7,12 @@ import cleanEventDetail, { CLEAN_EVENT_DETAIL } from './cleanEventDetail';
 import getEvents, { GET_EVENTS } from './getEvents';
 import getNameEvent, { GET_NAME_EVENT } from './getNameEvent';
 import postStage, { POST_STAGE } from './postStage';
+import registerUser, { REGISTER_USER } from './registerUser';
+import postArtist, {POST_ARTIST} from './postRegisterArtist';
+import loginUser, { LOGIN_USER } from './loginUser';
+import loginToken, { LOGIN_TOKEN } from './loginToken';
+import clearAuthError, { CLEAR_AUTH_ERR } from './clearAuthErr';
+import logout, { LOGOUT } from './logout';
 
 export const actions = {
     GET_EVENTS,
@@ -15,7 +21,13 @@ export const actions = {
     FILT_EVENTS,
     CLEAN_EVENT_DETAIL,
     GET_NAME_EVENT,
-    POST_STAGE
+    POST_STAGE,
+    REGISTER_USER,
+    POST_ARTIST,
+    LOGIN_USER,
+    LOGIN_TOKEN,
+    CLEAR_AUTH_ERR,
+    LOGOUT
 }
 
 const actionsCreator = {
