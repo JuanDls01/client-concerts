bff708a742b899e05d143332bab2a5bd785bedae
nicolas palomares <nicolaspalomares360@gmail.com>
2022-04-22 02:44:52 -0300

    feat/dashboardSeller : se agrega componente de lo que ve el vendedor

---
+++ src/components/DashboardSeller/CardAnalitics.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import style from "./CardAnalitics.module.css"
+
+export default function CardAnalitics({title,analitics,estadistics,img}) {
+  return (
+    <div className={style.contentAnalitics}>
+          <div className={style.cardAnalatics}>
+            <img src={img} className={style.imgCard} ></img>
+            <div className={style.analitics}>
+                <h3>{title}</h3>
+                 <h2>{analitics}</h2>
+            </div>
+              {estadistics?
+              <div className={style.estadistics} >{estadistics}</div>
+                :<p></p>}
+          </div> 
+      </div>
+  )
+}
