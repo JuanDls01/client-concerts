571b9c6f99005b149bf924966d34e1a304f08d2a
Jorge Vega <vegajor1112@gmail.com>
2022-04-25 03:08:04 -0300

    cambio de rutas

--- src/redux/actions/getNameEvent.js
+++ src/redux/actions/getNameEvent.js
@@ -1,24 +1,25 @@
-import axios from 'axios';
+import axios from "axios";
 
-export const GET_NAME_EVENT = 'GET_NAME_EVENT';
+export const GET_NAME_EVENT = "GET_NAME_EVENT";
 
 const getNameEvent = (event, artist, stage) => {
-    console.log(event)
-    console.log(artist)
-    console.log(stage)
-    return async (dispatch) => {
-        try {
-            const json = await axios.get(`http://localhost:3001/event/search?event=${event}&artist=${artist}&stage=${stage}`);
-            return dispatch({
-                type: 'GET_NAME_EVENT',
-                payload: json.data
-            })
-        }
-        catch (error) {
-            console.log(error);
-        }
+  console.log(event);
+  console.log(artist);
+  console.log(stage);
+  return async (dispatch) => {
+    try {
+      const json = await axios.get(
+        `/event/search?event=${event}&artist=${artist}&stage=${stage}`
+      );
+      return dispatch({
+        type: "GET_NAME_EVENT",
+        payload: json.data,
+      });
+    } catch (error) {
+      console.log(error);
     }
-}
+  };
+};
 
 // const getNameEvent = (payload) => {
 //     console.log(payload)
--- src/redux/actions/loginUser.js
+++ src/redux/actions/loginUser.js
@@ -1,22 +1,21 @@
 import axios from "axios";
-export const LOGIN_USER = 'LOGIN_USER';
+export const LOGIN_USER = "LOGIN_USER";
 
-const loginUser = input => {
-    // console.log(input);
+const loginUser = (input) => {
+  // console.log(input);
 
-        return async dispatch => {
-            try {
-                const json = await axios.post("http://localhost:3001/auth/login", input);
-            // console.log(json.data)
-            return dispatch({
-                type:LOGIN_USER,
-                payload:json.data
-            });
-            } catch (error) {
-                console.log(error)
-            }
-        }
-   
-}
+  return async (dispatch) => {
+    try {
+      const json = await axios.post("/auth/login", input);
+      // console.log(json.data)
+      return dispatch({
+        type: LOGIN_USER,
+        payload: json.data,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+};
 
 export default loginUser;
