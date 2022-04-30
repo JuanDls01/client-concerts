b8571c1955d29999b115e4f9f577e0820957ee52
Jorge Vega <vegajor1112@gmail.com>
2022-04-29 23:34:11 -0300

    paypal

--- src/redux/actions/savePreference.js
+++ src/redux/actions/savePreference.js
@@ -3,7 +3,6 @@ export const SAVE_PREFERENCE = "SAVE_PREFERENCE";
 
 export default function savePreference(preference) {
   return async function (dispatch) {
-    const preferenceId = await axios.post("/mercadoPago", preference);
-    dispatch({ type: SAVE_PREFERENCE, payload: preferenceId.data });
+    dispatch({ type: SAVE_PREFERENCE, payload: preference });
   };
 }
