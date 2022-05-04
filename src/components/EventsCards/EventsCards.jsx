842ce12d3c87af498ab7fa3c1038a62e3ee09555
davidvazgon26 <davidvazgon@hotmail.com>
2022-05-04 03:26:24 -0500

    estilos al componente de pago

--- src/components/EventsCards/EventsCards.jsx
+++ src/components/EventsCards/EventsCards.jsx
@@ -24,7 +24,7 @@ const EventsCards = () => {
     // console.log("hola");
     dispatch(getEvents());
     if (token === "") {
-      EVENTSPERPAGE = 9;
+      EVENTSPERPAGE = 6;
     } else EVENTSPERPAGE = 9;
   }, [dispatch, getEvents, token]);
 
